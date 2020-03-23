79bf654c8c29685bae72e54a491240b2416633c0
kohikeuchi <60934474+kohikeuchi@users.noreply.github.com>
2020-03-23 10:42:29 +0900

    modified

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -13,9 +13,9 @@ class Post(models.Model):
 
 
 
-def publish(self):
-    self.published_date= timezone.now()
-    self.save()
+    def publish(self):
+        self.published_date= timezone.now()
+        self.save()
 
-def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
